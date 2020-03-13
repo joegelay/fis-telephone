321b7c3196ca961f98abe14bd5918c60a4650ec7
joegelay <joegelay@gmail.com>
2020-03-13 15:31:14 -0600

    add DOMContentLoaded event listener, fetchCharacters

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,22 @@
-//your code here
+function fetchCharacters() {
+    fetch("https://telephone-assessment.herokuapp.com/api/v1/contacts")
+    .then(response => response.json())
+    .then(json => renderCharacters(json["data"]));
+};
+
+function renderCharacters(json) {
+    const list = document.getElementById('characters')
+    json.forEach(character => {
+      const li = document.createElement('li')
+
+      li.innerHTML = `<li>${character.name}</li>`
+      
+
+      list.appendChild(li)
+    })
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchCharacters()
+  })
+  
