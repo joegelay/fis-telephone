b1e0216aebb725063d04ed6a366f87d0192fdde8
joegelay <joegelay@gmail.com>
2020-03-13 15:40:19 -0600

    render characters

--- scripts/app.js
+++ scripts/app.js
@@ -6,17 +6,33 @@ function fetchCharacters() {
 
 function renderCharacters(json) {
     const list = document.getElementById('characters')
+
     json.forEach(character => {
       const li = document.createElement('li')
-
       li.innerHTML = `<li>${character.name}</li>`
-      
 
+      const img = document.createElement('img')
+      img.src = character.imageURL
+
+      const span = document.createElement('span')
+      span.innerHTML = `${character.name} - ${character.phone}`
+
+      const p = document.createElement('p')
+      p.innerHTML = character.message
+
+      const a = document.createElement('a')
+      a.innerHTML = `Leave ${character.name} a message`
+      a.setAttribute('character', character.name)
+      a.setAttribute('href', `contact.html?character=${character.name}`)
+      
       list.appendChild(li)
+      li.appendChild(img)
+      li.appendChild(span)
+      li.appendChild(p)
+      li.appendChild(a)
     })
-  }
+  };
   
   document.addEventListener('DOMContentLoaded', function() {
     fetchCharacters()
   })
-  
